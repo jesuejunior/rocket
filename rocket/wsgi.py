e7fc2ea549c7d9d4d2dd6403cd3892dc26e27c4f
Jesue Junior <jesuesousa@gmail.com>
2016-04-18 22:19:10 -0300

    Erasing files to be use in the future

--- rocket/wsgi.py
+++ rocket/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for rocket project.
+#!/usr/bin/env python
+# encoding: utf-8
+from rocket.app import app
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rocket.settings")
-
-application = get_wsgi_application()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
