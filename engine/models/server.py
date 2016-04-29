55c8b3ed2bce8d89cb7db4f6f65d2703d346d1cd
Jesue Junior <jjunior@muxi.com.br>
2016-04-29 18:47:51 -0400

    Initial model of server

--- engine/models/server.py
+++ engine/models/server.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from flask.ext.restful.fields import Integer, String
 from sqlalchemy import Column
+from sqlalchemy_utils import IPAddressType, PasswordType
 
 from engine.models import Model
 
@@ -10,8 +11,17 @@ class Server(Model):
     """
 
     """
-
     __tablename__ = 'server'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(155), nullable=False)
+    ip = Column(IPAddressType)
+    username = Column(String(155), nullable=False)
+    password = Column(PasswordType(
+        schemes=[
+            'pbkdf2_sha512',
+            'md5_crypt'
+        ],
+        deprecated=['md5_crypt']
+    ))
+    private_key = Column()
