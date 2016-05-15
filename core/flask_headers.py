19bca7d893a2af145295aa466835788447323304
Jesue Junior <jesuesousa@gmail.com>
2016-05-15 16:37:59 -0300

    Adding some functions to help

---
+++ core/flask_headers.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+from functools import wraps
+
+from flask import make_response
+
+
+def add_headers(headers={}):
+    """This decorator adds the headers passed in to the response"""
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+            h = resp.headers
+            for header, value in headers.items():
+                h[header] = value
+            return resp
+        return decorated_function
+    return decorator
---
+++ core/func_dict.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
