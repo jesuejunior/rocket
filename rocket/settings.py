9ab25b81dfa7907edd13520463d7d1691a23f338
Jesue Junior <jjunior@muxi.com.br>
2016-06-06 17:49:18 -0400

    cleaning settings

--- rocket/settings.py
+++ rocket/settings.py
@@ -15,16 +15,3 @@ app.config.from_object(__name__)
 api = Api(app)
 
 api_version = 'v1'
-
-# Deploy endpoints
-api.add_resource(DeployResource, '/deploy/new',)
-
-# Container endpoints
-api.add_resource(ContainerResource, '/container/all',)
-
-# Server endpoints
-api.add_resource(ServerResource, '/{0}/servers'.format(api_version),
-				 '/{0}/servers/<int:id>'.format(api_version))
-
-
-
