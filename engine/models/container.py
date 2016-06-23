9b3b5649ec0d7ec4517d056138f618c00145a846
Jesue Junior <jesuesousa@gmail.com>
2016-06-23 14:42:17 -0300

    Fixing import and adding autoincrement

--- engine/models/container.py
+++ engine/models/container.py
@@ -1,6 +1,5 @@
 # encoding: utf-8
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from sqlalchemy_utils import JSONType
 
 from rocket.settings import Model
@@ -12,8 +11,6 @@ class Container(Model):
     """
     __tablename__ = 'container'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(40))
     config = Column(JSONType)
-    spacebus_id = Column(Integer, ForeignKey('spacebus.id'))
-    spacebus = relationship("Spacebus", back_populates="spacebus")
