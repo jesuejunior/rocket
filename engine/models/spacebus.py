1ffccf824e5e6109d1676eb67d5a58ca124325f3
Jesue Junior <jesuesousa@gmail.com>
2016-06-28 17:22:02 -0400

    Improving relationship between spacebus and container

--- engine/models/spacebus.py
+++ engine/models/spacebus.py
@@ -1,16 +1,21 @@
 # encoding: utf-8
 from django.db import models
 
+from engine.models.container import Container
+
 
 class Spacebus(models.Model):
     """
     """
     name = models.CharField(max_length=40)
-    container = models.ManyToManyField('Container',
+    container = models.ManyToManyField(Container,
                                        related_name='spacebus',
                                        db_table='spacebus_has_container',
                                        null=True,
                                        blank=True)
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = u'spacebus'
         verbose_name = 'Spacebus'
