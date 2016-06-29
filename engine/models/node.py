74e274a0d2f55b8e86a72f036aa7a171265a77e9
Jesue Junior <jesuesousa@gmail.com>
2016-06-28 23:01:41 -0300

    Adding node serialize

--- engine/models/node.py
+++ engine/models/node.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from django.db import models
 
+PROVIDERS = ('aws', 'rackspace', 'vmware', 'digital ocean', 'linode', )
 
 class Node(models.Model):
 
@@ -8,7 +9,7 @@ class Node(models.Model):
     """
     name = models.CharField(max_length=150)
     so = models.CharField(max_length=40)
-    provider = models.CharField(max_length=20)
+    provider = models.CharField(choices=PROVIDERS, max_length=20, default='digital ocean')
     ip = models.IPAddressField()
     fqdn = models.CharField(max_length=50, null=True, blank=True)
     username = models.CharField(max_length=50)
@@ -19,17 +20,3 @@ class Node(models.Model):
     class Meta:
         db_table = u'node'
         verbose_name = 'Node'
-
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'so': self.so,
-            'provider': self.provider,
-            'ip': str(self.ip),
-            'fqdn': self.fqdn,
-            'username': self.username,
-            'password': self.password,
-            'private_key': self.private_key
-        }
