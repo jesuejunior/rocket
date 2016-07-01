b3cef0dd2b5b2a703ce4a563cab9153e023bc24f
Jesue Junior <jesuesousa@gmail.com>
2016-07-01 18:27:54 -0300

    Adding profile app with user model

---
+++ profile/models/__init__.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+
+from profile.models.user import *
---
+++ profile/models/user.py
@@ -0,0 +1,92 @@
+# encoding: utf-8
+__author__ = 'jesuejunior'
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, is_active=True, password=None):
+        """
+        email: email address
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            is_active=is_active,
+            )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email=email,  password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True, )
+    first_login = models.BooleanField(default=True)
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name='Data de Nascimento')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_username(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    class Meta:
+        db_table = u'user'
+        app_label = 'api'
+        verbose_name = u'Usuário'
