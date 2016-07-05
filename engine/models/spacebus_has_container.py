d147a7c7822aab9ca2d3112c0d0ebdb3435ed87c
Jesue Junior <jesuesousa@gmail.com>
2016-07-05 08:22:48 -0300

    Commeting temporarily many to many spacebus_has_container

--- engine/models/spacebus_has_container.py
+++ engine/models/spacebus_has_container.py
@@ -1,16 +1,16 @@
-# encoding: utf-8
-from django.db import models
-
-from engine.models.container import Container
-from engine.models.spacebus import Spacebus
-
-
-class SpacebusHasContainer(models.Model):
-
-    """
-    """
-    spacebus = models.ForeignKey(Spacebus)
-    container = models.ForeignKey(Container)
-
-    class Meta:
-        db_table = u'spacebus_has_container'
+# # encoding: utf-8
+# from django.db import models
+#
+# from engine.models.container import Container
+# from engine.models.spacebus import Spacebus
+#
+#
+# class SpacebusHasContainer(models.Model):
+#
+#     """
+#     """
+#     spacebus = models.ForeignKey(Spacebus)
+#     container = models.ForeignKey(Container)
+#
+#     class Meta:
+#         db_table = u'spacebus_has_container'
