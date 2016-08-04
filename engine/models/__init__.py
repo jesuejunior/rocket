f6016829d85b6ac9c243f945981ee1e1cf619748
Jesue Junior <jesuesousa@gmail.com>
2016-08-04 17:36:56 -0300

    #6  Renaming project grom rocket to everest p3

--- engine/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-from engine.models.container import *
-from engine.models.node import *
-from engine.models.spacebus import *
-# from engine.models.spacebus_has_container import *
