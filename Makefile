1ccadbc1dd602197325518a585be092bbf635842
Jesue Junior <jesuesousa@gmail.com>
2016-09-04 19:22:47 -0300

    Fix #4 endpont with test created with auth by JWT

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 test:
 	@pip install -r requirements_dev.txt
-	@docker-compose -f docker-compose-test.yml down  > /dev/null 2>&1 
+	#@docker-compose -f docker-compose-test.yml rm --rmi local  > /dev/null 2>&1
 	@docker-compose -f docker-compose-test.yml up -d
 	@echo "Waiting database stay ready"
 	@sleep 5
 	@py.test src/ --create-db --flakes
 
 dev:
+	@pip install --upgrade pip
 	@pip install -r requirements_dev.txt
 	@docker-compose -f docker-compose.yml up -d
+	@sleep 3
+	@python src/manage.py migrate
 	@python src/manage.py runserver 0.0.0.0:8000
 	@echo "Running server in dev mode"
 
-.PHONY: dev test
+.PHONY: test dev
--- src/nepal/tests/test_node.py
+++ src/nepal/tests/test_node.py
@@ -13,15 +13,15 @@ from profile.models.user import User
 class NodeTest:
     @pytest.fixture(autouse=True)
     def setUp(self, client):
-        mommy.make_one(User, email='test@adm.cc', password='test123')
-        
-        token = client.post('/users/login', 
+        self.client = client
+        self.user = User.objects.create_user(email='test@adm.cc', password='test123')
+        self.headers={'Content-Type': 'application/json'}
+        response = self.client.post('/users/login',  headers=self.headers,
             data={'email': 'test@adm.cc', 'password': 'test123'})
-        import ipdb; ipdb.set_trace()
 
-        self.headers={'Content-Type': 'application/json',
-                'Authorization': 'JWT {0}'.format(token['token'])}
-        
+        token = response.data.get('token')
+        # self.auth = {'Authorization': 'JWT {0}'.format(token)}
+        self.headers.update( {'HTTP_AUTHORIZATION': 'JWT {0}'.format(token)})
         node1 = {
             'id': 100,
             'name': 'node1',
@@ -35,9 +35,13 @@ class NodeTest:
         mommy.make_one(Node, **node1)
         mommy.make_many(Node, 9)
 
+    @pytest.mark.django_db(transaction=True)
+    def test_try_get_nodes_without_auth(self):
+        response = self.client.get('/nodes', content_type='application/json')
+        assert 401 == response.status_code
 
     @pytest.mark.django_db(transaction=True)
-    def test_create_new_node(self, client):
+    def test_create_new_node(self):
         data = {
             'name': 'node_test',
             'so': 'centos',
@@ -48,23 +52,25 @@ class NodeTest:
             'password': 'abc1234'
         }
 
-        result = client.post('/nodes', data=json.dumps(data), headers=self.headers)
+        response = self.client.post('/nodes', data=json.dumps(data),
+                content_type='application/json', **self.headers)
         # TODO: must be assert more things
-        assert 201 == result.status_code
-        assert 'centos' == result.data.get('so')
+        assert 201 == response.status_code
+        assert 'centos' == response.data.get('so')
 
     @pytest.mark.django_db(transaction=True)
-    def test_get_node_all(self, client):
-        req = client.get('/nodes', headers=self.headers )
-        assert 10 == len(req.data)
+    def test_get_node_all(self):
+        response = self.client.get('/nodes', **self.headers )
+        assert 200 == response.status_code
+        assert 10 == len(response.data)
 
     @pytest.mark.django_db(transaction=True)
-    def test_get_node_by_id(self, client):
-        req = client.get('/nodes/1', headers=self.headers )
-        result = req.data
+    def test_get_node_by_id(self):
+        response = self.client.get('/nodes/100', **self.headers )
+        result = response.data
 
-        assert 200 == req.status_code
-        assert 1 == result.get('id')
+        assert 200 == response.status_code
+        assert 100 == result.get('id')
         assert 'node1' == result.get('name')
         assert 'vmware' == result.get('provider')
         assert '192.168.1.250' == result.get('ip')
@@ -72,7 +78,7 @@ class NodeTest:
         assert 'abc123' == result.get('password')
 
     @pytest.mark.django_db(transaction=True)
-    def test_update_node_ok(self, client):
+    def test_update_node_ok(self):
         data = {
             'name': 'node1',
             'so': 'centos7',
@@ -83,7 +89,8 @@ class NodeTest:
             'password': 'cba123'
         }
 
-        req = client.put('/nodes/1', data=json.dumps(data), headers=self.headers)
+        req = self.client.put('/nodes/100', data=json.dumps(data),
+                content_type='application/json', **self.headers)
         result = req.data
 
         assert 200 == req.status_code
@@ -93,16 +100,17 @@ class NodeTest:
         assert '201.18.1.100' == str(result.get('ip'))
 
     @pytest.mark.django_db(transaction=True)
-    def test_try_update_node_not_found(self, client):
-        req = client.put('/nodes/132', data=json.dumps({}), headers=self.headers)
-        assert 404 == req.status_code
+    def test_try_update_node_not_found(self):
+        response = self.client.put('/nodes/132', data=json.dumps({}),
+                content_type='application/json',  **self.headers)
+        assert 404 == response.status_code
 
     @pytest.mark.django_db(transaction=True)
-    def test_delete_node_ok(self, client):
-        result = client.delete('/nodes/1', headers=self.headers)
+    def test_delete_node_ok(self):
+        result = self.client.delete('/nodes/100', **self.headers)
         assert 204 == result.status_code
 
     @pytest.mark.django_db(transaction=True)
-    def test_try_delete_node_that_not_exist(self, client):
-        result = client.delete('/nodes/122', headers=self.headers)
+    def test_try_delete_node_that_not_exist(self):
+        result = self.client.delete('/nodes/122', **self.headers)
         assert 404 == result.status_code
