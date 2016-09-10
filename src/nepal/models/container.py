14c07c343532525d5790d3795a72b024153239e5
Jesue Junior <jesuesousa@gmail.com>
2016-09-09 22:43:33 -0300

    #9  Adding container on many nodes

--- src/nepal/models/container.py
+++ src/nepal/models/container.py
@@ -9,7 +9,7 @@ class Container(models.Model):
     """
     name = models.CharField(max_length=50, verbose_name='Naem of container')
     nodes = models.ManyToManyField(Node, db_table='container_has_node',
-            related_name='containers',verbose_name='Container', blank=True)
+            related_name='containers',verbose_name='Container')
     config = JSONField()
 
     class Meta:
